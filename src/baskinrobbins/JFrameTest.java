ee55f81d49852818d07efb90f2a000a5ccca11ac
admin <admin@DESKTOP-TUK5P2A.mshome.net>
2022-10-31 12:13:15 +0900

    임시 저장

--- src/baskinrobbins/JFrameTest.java
+++ src/baskinrobbins/JFrameTest.java
@@ -1,31 +1,77 @@
 package baskinrobbins;
 
 import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class JFrameTest extends JFrame {
+public class JFrameTest extends JFrame implements ActionListener,MouseListener {
+	
+	static int buttonNumber = 5;
+	static JButton buttonList[] = new JButton[buttonNumber];
+	static String strList[]= {"게임 시작","다른 숫자","규칙 설명","전적 확인","게임 종료"};
+	
 	JFrameTest()
 	{
-		setTitle("첫번째 프레임");
-		setSize(300,400);
+		setTitle("BaskinRobbins 31 Game");
+		setSize(500,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c = getContentPane();
-		c.setLayout(new FlowLayout());
-	
-		JButton j = new JButton("확인");
-		JButton j2 = new JButton("취소");
-
-		c.add(j);
-		c.add(j2);
+		Container c = getContentPane();	
+		c.setLayout(new GridLayout(buttonNumber,1,10,10));
 		
+		for(int i = 0; i<buttonNumber; i++) {
+			buttonList[i] = new JButton(strList[i]);
+			buttonList[i].setFont(new Font("SansSerif",Font.BOLD,40));
+			c.add(buttonList[i]);
+		}
+	
 		setVisible(true);
 
 	}
 
 	public static void main(String[] args) {
 		new JFrameTest();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
